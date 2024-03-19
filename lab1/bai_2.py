a1814aeff38fad700d0077e69002d07cca05f247
39_Nguyen_Thai_0053 <148730991+nthai21@users.noreply.github.com>
2024-03-19 19:26:36 +0700

    project2

--- lab1/bai_2.py
+++
@@ -1,7 +0,0 @@
-so_sach=input("Mời nhập vào số lượng sách:")
-ma_sach=input("Mời nhập vào mã sách:")
-ten_sach=input("Mời nhập vào tên sách:")
-tac_gia=input("Mời nhập vào tên tác giả:")
-xuat_ban=input("Mời nhập vào năm xuất bản:")
-print("Thư viện ĐHKTKTCN có:",so_sach,"sách",ten_sach,"với mã số sách",ma_sach,".Cuốn sách của tác giả ",tac_gia,
-      "được xuất bản vào năm ",xuat_ban)
