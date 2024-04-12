3c8d64f457afe0343cb56f73412c71fa0bf01cee
39_Nguyen_Thai_0053 <148730991+nthai21@users.noreply.github.com>
2024-04-12 11:46:50 +0700

    Create Bai6.py

---
+++ lab5/Bai6.py
@@ -0,0 +1,8 @@
+'''Viết một chương trình Python nhập một chuỗi từ bàn phím và thực hiện các
+yêu cầu sau:
++ Kiểm tra xem mỗi ký tự trong chuỗi có phải là một ký tự đặc biệt (không phải
+ký tự chữ hoặc số) không
++ Nếu có, đếm số lần xuất hiện của từng ký tự đặc biệt trong chuỗi.
++ Tính tỷ lệ phần trăm của mỗi ký tự đặc biệt trong chuỗi so với tổng số ký tự
+trong chuỗi.'''
+print("------------------------------Bài làm--------------------------------")
