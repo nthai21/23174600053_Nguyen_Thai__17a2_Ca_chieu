e602b051f6045d1d9cb7c9f9a4e4bd184af0607b
39_Nguyen_Thai_0053 <148730991+nthai21@users.noreply.github.com>
2024-04-19 23:06:05 +0700

    Create lab6.1.py

---
+++ lab6/lab6.1.py
@@ -0,0 +1,9 @@
+'''Viết một chương trình Python để thực hiện các thao tác sau trên ma trận:
+Bài 6.1:
++ Nhập Ma trận: Cho phép người dùng nhập các phần tử của một ma trận có
+kích thước m x n từ bàn phím.
++ Tổng của Ma trận: Tính tổng của tất cả các phần tử trong ma trận.
++ Tích của Ma trận: Cho phép nhập một ma trận thứ hai và tính tích của hai ma
+trận (nếu có thể).
++ Ma trận chuyển vị: Tạo và in ra ma trận chuyển vị của ma trận ban đầu'''
+
