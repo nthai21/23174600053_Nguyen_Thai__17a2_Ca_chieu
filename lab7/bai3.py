84217a2e00a1682075df3b28d8d474d4edb8742e
39_Nguyen_Thai_0053 <148730991+nthai21@users.noreply.github.com>
2024-05-03 08:02:49 +0700

    Create bai3.py

---
+++ lab7/bai3.py
@@ -0,0 +1,5 @@
+'''Sử dụng từ điển làm bài tập sau:
+Chọn một văn bản tiếng Anh trong đường link (https://englishcbse.blogspot.com/2011/11/list-of-short-paragraphs50-to-80words.html)
+Đếm số lượng các từ giống nhau trong văn bản vừa nhập.
+In ra màn hình từ có số lượng nhiều nhất và ít nhất.'''
+print('-----------------------------------Bài làm--------------------------------')
